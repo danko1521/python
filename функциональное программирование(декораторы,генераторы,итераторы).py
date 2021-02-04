566638ca1caaa16876bab995863f3f83e449f620
danko1521 <vip.danko1521@mail.ru>
2021-02-04 19:25:54 +0300

    маленькие кодики(2) с фибонашичками, +итератор  (пример) +декоратор (пример)

--- функциональное программирование(декораторы,генераторы,итераторы).py
+++ функциональное программирование(декораторы,генераторы,итераторы).py
@@ -11,30 +11,60 @@
 # print(fib)
 # for value in fib:
 #     print(value)
-# ____________________________________________фибоначи_________________________________
 
 
-class Fibonacci:
-    """Итератор последовательности фибоначчи до N элементов"""
+# class Fibonacci:
+#     """Итератор последовательности фибоначчи до N элементов"""
+#
+#     def __init__(self, n):
+#         self.i, self.a, self.b, self.n = 0, 0, 1, n
+#
+#     def __iter__(self):
+#         self.i, self.a, self.b, = 0, 0, 1
+#         return self
+#
+#     def __next__(self):
+#         self.i += 1
+#         if self.i > 1:
+#             if self.i > self.n:
+#                 raise StopIteration()
+#             self.a, self.b = self.b, self.a + self.b
+#         return self.a
+#
+#
+# fib_iterator = Fibonacci(n=10)
+# print(fib_iterator)
+# for value in fib_iterator:
+#     print(value)
+# print(13 in fib_iterator)
 
-    def __init__(self, n):
-        self.i, self.a, self.b, self.n = 0, 0, 1, n
-
-    def __iter__(self):
-        self.i, self.a, self.b, = 0, 0, 1
-        return self
-
-    def __next__(self):
-        self.i += 1
-        if self.i > 1:
-            if self.i > self.n:
-                raise StopIteration()
-            self.a, self.b = self.b, self.a + self.b
-        return self.a
+# ____________________________________________Итератор_________________________________
 
 
-fib_iterator = Fibonacci(n=10)
-print(fib_iterator)
-for value in fib_iterator:
-    print(value)
-print(13 in fib_iterator)
+import time
+
+
+def time_track(func, *args, **kwargs):
+    started_at = time.time()
+
+    result = func(*args, **kwargs)
+
+    ended_at = time.time()
+    elapsed = round(ended_at - started_at, 4)
+    print(f'Функция работала {elapsed} секунд(ы)')
+    return result
+
+
+def digits(*args):
+    total = 1
+    for number in args:
+        total *= number ** 5000
+    return len(str(total))
+
+
+result = time_track(digits, 3141, 5926, 2718, 2818)
+print(result)
+
+
+
+#--------------------------------Декоратор------------------------------------------------
